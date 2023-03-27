88365da65e47f76f64ec61ec2dd1f1289b68e592
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-03-27 15:36:34 +0400

    Water Pump and Drill emulation with threads and syncronized Tank.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.company.others.WaterPump;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,11 +9,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	List<Integer> list = new ArrayList<>();
-        Collections.addAll(list, 1,5,2,3,7,3,8,9);
-        Integer pos = Integer.valueOf(3);
-        list.remove(pos);
-        System.out.println(list);
-
+        try{
+            WaterPump pump = new WaterPump();
+            pump.balanceIt();
+        } catch (InterruptedException e) {}
     }
 }
