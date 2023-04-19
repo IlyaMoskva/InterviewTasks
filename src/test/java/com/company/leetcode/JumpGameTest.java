90da5f01e40b569914404329a4157a4db51f63db
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-04-19 17:05:05 +0400

    LC 55: Jump Game

---
+++ src/test/java/com/company/leetcode/JumpGameTest.java
@@ -0,0 +1,26 @@
+package com.company.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JumpGameTest {
+    JumpGame jg = new JumpGame();
+
+    public static Object[][] data() {
+        return new Object[][] {
+                new Object [] {true, new int[] {2,3,1,1,4}},
+                new Object [] {false, new int[] {3,2,1,0,4}},
+                new Object [] {false, new int[] {0, 3}},
+                new Object [] {true, new int[] {0}}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void canJump(boolean expected, int[] nums) {
+        assertEquals(expected, jg.canJump(nums));
+    }
+}
