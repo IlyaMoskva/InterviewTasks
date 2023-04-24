8f2a41d5784c3f0325ccaf90a6f48b7e63d65e50
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-04-24 16:11:15 +0400

    LC 242: Valid anagram

---
+++ src/test/java/com/company/leetcode/ValidAnagrammTest.java
@@ -0,0 +1,26 @@
+package com.company.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidAnagrammTest {
+
+    public static Object[][] data() {
+        return new Object[][]{
+                {"anagram", "nagaram", true},
+                {"rat", "car", false},
+                {"", "", true},
+                {"", "a", false},
+                {"a", "", false},
+                {"a", "a", true}
+        };
+
+    }
+    @ParameterizedTest
+    @MethodSource("data")
+    void isAnagram(String s, String t, boolean expected) {
+        assertEquals(expected, new ValidAnagramm().isAnagram(s, t));
+    }
+}
