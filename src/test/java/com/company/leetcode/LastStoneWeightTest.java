a02fd6d2382faab7e145267145a559671a624fea
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-04-24 11:30:22 +0400

    LC: 1046: Last Stone Weight - beats 98%

---
+++ src/test/java/com/company/leetcode/LastStoneWeightTest.java
@@ -0,0 +1,27 @@
+package com.company.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LastStoneWeightTest {
+
+    LastStoneWeight lsw = new LastStoneWeight();
+    public static Object[][] data() {
+        return new Object[][]{
+                new Object[]{1, new int[]{2, 7, 4, 1, 8, 1}},
+                new Object[]{1, new int[]{3, 2, 1, 1, 4}},
+                new Object[]{2, new int[]{1, 3}},
+                new Object[]{1, new int[]{1}},
+                new Object[]{0, new int[]{2,2}},
+                new Object[]{3, new int[]{7,6,7,6,9}}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void lastStoneWeight(int expected, int[] stones) {
+        assertEquals(expected, lsw.lastStoneWeight(stones));
+    }
+}
