4f9d9d1b4d6fa661ea2a99b81ed1a7d4cfb793d3
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-04-28 13:19:44 +0400

    LC 2: Add Two Numbers + ListNode structure

---
+++ src/main/java/com/company/leetcode/AddTwoNumbers.java
@@ -0,0 +1,45 @@
+package com.company.leetcode;
+
+import com.company.leetcode.structures.ListNode;
+
+/**
+ * LC 2: Add Two Numbers
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ * Example 1:
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+
+ * Example 2:
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+
+ * Example 3:
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        if (carry > 0) {
+            p.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
---
+++ src/main/java/com/company/leetcode/structures/ListNode.java
@@ -0,0 +1,18 @@
+package com.company.leetcode.structures;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode(int[] vals) {
+        this.val = vals[0];
+        ListNode head = this;
+        for (int i = 1; i < vals.length; i++) {
+            head.next = new ListNode(vals[i]);
+            head = head.next;
+        }
+    }
+}
