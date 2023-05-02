dec5da586bbf00b1747c1da364ba33450d321295
IlyaMoskva <ilya.moskvitin@talabat.com>
2023-05-02 17:45:33 +0400

    LC 8. String to Integer (atoi)

---
+++ src/test/java/com/company/leetcode/AtoiTest.java
@@ -0,0 +1,32 @@
+package com.company.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtoiTest {
+
+    static Object[][] data() {
+        return new Object[][]{
+                new Object[]{"42", 42},
+                new Object[]{"", 0},
+                new Object[]{"   ", 0},
+                new Object[]{"+1", 1},
+                new Object[]{"   -42", -42},
+                new Object[]{"   +42", 42},
+                new Object[]{"   -00042", -42},
+                new Object[]{"4193 with words", 4193},
+                new Object[]{"words and 987", 0},
+                new Object[]{"-91283472332", -2147483648},
+                new Object[]{"3.14159", 3}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void myAtoi(String s, int expected) {
+        assertEquals(expected, new Atoi().myAtoi(s));
+    }
+
+}
