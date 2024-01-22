5ec5545c1dab0f763fe9e2ba0ad80448627e41b4
IlyaMoskva <i-moskva@ya.ru>
2024-01-22 17:08:55 +0400

    2462. Total Cost to Hire K Workers Test corrected.

--- src/test/java/com/company/leetcode/numbers/TotalCostWorkersTest.java
+++ src/test/java/com/company/leetcode/numbers/TotalCostWorkersTest.java
@@ -8,15 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class TotalCostWorkersTest {
     public static Object[][] data() {
         return new Object[][]{
-                //new Object[]{11, 3, 4, new int[]{17,12,10,2,7,2,11,20,8}},
+                new Object[]{11, 3, 4, new int[]{17,12,10,2,7,2,11,20,8}},
                 new Object[]{4, 3, 3, new int[]{1,2,4,1}}
         };
     }
 
     @ParameterizedTest
     @MethodSource("data")
-    public void findMaxAverageTest(double totalCost, int rounds, int candidates, int[] costs) {
+    public void totalCostTest(double totalCost, int rounds, int candidates, int[] costs) {
         assertEquals(totalCost, new TotalCostWorkers().totalCost(costs, rounds, candidates));
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void totalCostTwoHeapsTest(double totalCost, int rounds, int candidates, int[] costs) {
+        assertEquals(totalCost, new TotalCostWorkers().totalCostTwoHeaps(costs, rounds, candidates));
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void totalCostFastestTest(double totalCost, int rounds, int candidates, int[] costs) {
+        assertEquals(totalCost, new TotalCostWorkers().totalCostFastest(costs, rounds, candidates));
     }
 
 }
