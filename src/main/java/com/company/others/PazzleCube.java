beaea616c2e8aeecdef769a8f48eba4579310933
IlyaMoskva <i-moskva@ya.ru>
2024-02-21 14:43:13 +0400

    Puzzle Cube draft

---
+++ src/main/java/com/company/others/PazzleCube.java
@@ -0,0 +1,20 @@
+package com.company.others;
+
+public class PazzleCube {
+
+    public static class Tile {
+        public int[][] borders = new int[4][];
+        public Tile(int[][] borders) {
+            this.borders = borders;
+        }
+        public void rotate() {
+
+        }
+    }
+
+    public boolean isCube(Tile[] tiles) {
+
+
+        return true;
+    }
+}
---
+++ src/test/java/com/company/others/PazzleCubeTest.java
@@ -0,0 +1,10 @@
+package com.company.others;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PazzleCubeTest {
+
+    //int[][] borders = new int[]{ new int[]{0,1,1}, new int[]{} };
+    //PazzleCube.Tile tile = new PazzleCube.Tile(borders);
+
+}
